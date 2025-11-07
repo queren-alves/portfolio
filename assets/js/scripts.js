67559b96ca0737f24180f7a7ca9f65f4ad38eae9
Queren <querenhalves@outlook.com>
2025-11-07 10:51:23 -0300

    :sparkles: Validação e envio do Formulário

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,6 @@
 const about = document.querySelector("#about");
+const formulario = document.querySelector("#formulario");
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 async function getApiGithub(){
     try {
@@ -45,5 +47,44 @@ async function getApiGithub(){
         console.error(error);
     }
 }
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const campoNome = document.querySelector('#nome');
+    const txtNome = document.querySelector('#txtNome');
+
+    if(campoNome.value.length < 3) {
+        txtNome.innerHTML = 'Nome deve ter no mínimo 3 caracteres.';
+        campoNome.focus();
+        return;
+    } else {
+        txtNome.innerHTML = '';
+    }
+
+    const campoEmail = document.querySelector('#email');
+    const txtEmail = document.querySelector('#txtEmail');
+
+    if(!campoEmail.value.match(emailRegex)) {
+        txtEmail.innerHTML = 'Digite um e-mail válido.';
+        campoEmail.focus();
+        return;
+    } else {
+        txtEmail.innerHTML = '';
+    }
+
+    const campoAssunto = document.querySelector('#assunto');
+    const txtAssunto = document.querySelector('#txtAssunto');
+
+    if(campoAssunto.value.length < 3) {
+        txtAssunto.innerHTML = 'Assunto deve ter no mínimo 5 caracteres.';
+        campoAssunto.focus();
+        return;
+    } else {
+        txtAssunto.innerHTML = '';
+    }
+
+    formulario.submit();
+})
 
 getApiGithub();
